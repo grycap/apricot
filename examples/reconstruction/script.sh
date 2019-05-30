1afaa0fbb699d5297a926e68a98a0ccd43072b53
Vicent Giménez Alventosa <vicent.gimenez.alventosa@gmail.com>
2019-05-30 15:21:28 +0200

    Update script.sh

--- examples/reconstruction/script.sh
+++ examples/reconstruction/script.sh
@@ -18,13 +18,10 @@ echo "NVoxels: __0__ __0__ __1__"
 echo "N chunks: __2__"
 
 #Reconstruct image
-time ./reconstructor --LOR-format 0 --nvox __0__ __0__ __1__ --dvox `python -c "print(28.0/float(__0__))"` `python -c "prin
-t(28.0/float(__0__))"` `python -c "print(28.0/float(__1__))"` --nChunks __2__ --niter 1 --kdim 2 --sensitivity NONE /home/u
-buntu/input/completeSimulationData.dat
+time ./reconstructor --LOR-format 0 --nvox __0__ __0__ __1__ --dvox `python -c "print(28.0/float(__0__))"` `python -c "print(28.0/float(__0__))"` `python -c "print(28.0/float(__1__))"` --nChunks __2__ --niter 1 --kdim 2 --sensitivity NONE /home/ubuntu/input/completeSimulationData.dat
 
 #Compare result with reference image
-./evaluateImage normalizedCompleteData.txt __0__ __0__ __1__ `python -c "print(28.0/float(__0__))"` `python -c "print(28.0/
-float(__0__))"` `python -c "print(28.0/float(__1__))"`
+./evaluateImage normalizedCompleteData.txt __0__ __0__ __1__ `python -c "print(28.0/float(__0__))"` `python -c "print(28.0/float(__0__))"` `python -c "print(28.0/float(__1__))"`
 
 #Remove reconstructed image to save disk space
 rm normalizedCompleteData.txt
